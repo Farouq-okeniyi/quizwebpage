968da7fe9edd8d34e170ae150ef2391020ab62cb
DESKTOP-1M8BI2T\USER <farouqokeniyi2004@gmail.com>
2024-05-16 20:59:24 +0100

    started the Javascript and included the question arrays

---
+++ javascript/question.js
@@ -0,0 +1,59 @@
+//store questions and options
+let question = [
+    {
+        numb:1,
+        question: "What is the capital of Australia?",
+        answer:"Canberra",
+        Options:[
+            "Sydney",
+            "Canberra",
+            " Melbourne",
+            "Brisbane"
+        ]
+    },
+    {
+        numb:2,
+        question: "Who is known as the father of modern physics?",
+        answer:"Albert Einstein",
+        Options:[
+            " Isaac Newton",
+            "Albert Einstein",
+            "Galileo Galilei",
+            "Nikola Tesla"
+        ]
+    },
+    {
+        numb:3,
+        question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
+        answer:"Venus",
+        Options:[
+            "Jupiter",
+            "Mars",
+            "Venus",
+            "Saturn"
+        ]
+    },
+    {
+        numb:4,
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        answer:"Harper Lee",
+        Options:[
+            "J.K. Rowling",
+            "Mark Twain",
+            "Ernest Hemingway",
+            "Harper Lee"
+        ]
+    },
+    {
+        numb:5,
+        question: " Which of the following is a noble gas?",
+        answer:"Helium",
+        Options:[
+            "Carbon",
+            "Oxygen",
+            "Helium",
+            "Nitrogen"
+        ]
+    },
+    
+];
---
+++ javascript/script.js
@@ -0,0 +1,69 @@
+//making sure all form requrements are met
+const startbtn      =   document.getElementById("startbutton");
+const info          =   document.getElementById("infoboxid");
+const user          =   document.getElementById("nameid");
+let user_name       =   user.value;
+const quit          =   document.getElementById("quitid");
+const start_quiz    =   document.getElementById("continueid");
+const quizbox       =   document.getElementById("quizboxid")
+console.log(user.value);
+
+//if the start button is clicked
+startbtn.onclick = ()=>{
+    info.style.display="inline";
+   startbtn.style.display="none"
+}
+//if the quit button is clicked
+quit.onclick = () =>{
+    info.style.display="none";
+    startbtn.style.display="flex"
+}
+//if the continue button is clicked
+start_quiz.onclick = () =>{
+    user_name = user.value
+    let user_namelength = user_name.length
+    if(user_namelength == 0){
+        window.alert("enter a username")
+    }
+    else{
+        if(user_namelength < 12){
+            info.style.display="none"
+        quizbox.style.display="inline"
+        ShowQuestion(0);
+        }else{
+            window.alert("username is too long")
+        }
+        
+    }
+}
+
+let que_count = 0;
+
+// const next_question = document.getElementById("nextqueid");
+// console.log(next_question)
+//when next button is clicked
+
+// next_question.addEventListener("click", nextquestion);
+function nextquestion(){
+    console.log("hey");
+    if(que_count < question.length-1){
+        que_count++;
+        ShowQuestion(que_count)
+    }else{
+        window.alert('question completed')
+    };
+}
+
+//function to show the questions
+function ShowQuestion(index){
+    const que_text = document.getElementById("que_textid");
+    const optionlist = document.getElementById("optionlistid");
+    let que_tag = "<span>" + question[index].question +"</span>";
+     let option_tag =
+     '<div class="option"><span>'+ question[index].Options[0]+ '</span><div class="icon tick"><i class="fas fa-check"></i></div></div>'+
+      '<div class="option"><span>'+ question[index].Options[1]+ '</span><div class="icon cross"><i class="fas fa-times"></i></div></div>' +
+      '<div class="option"><span>'+ question[index].Options[2]+ '</span><div class="icon cross"><i class="fas fa-times"></i></div></div>' +
+      '<div class="option"><span>'+ question[index].Options[3]+ '</span><div class="icon cross"><i class="fas fa-times"></i></div></div>';
+    que_text.innerHTML = que_tag;
+     optionlist.innerHTML = option_tag;
+}
